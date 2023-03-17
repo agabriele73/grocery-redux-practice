5e5e62b1e5db11d67d5336c5c37a191af260c789
agabriele73 <agabrielejr@gmail.com>
2023-03-16 19:58:49 -0700

    phase 0-2 done

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+// ./src/store/index.js
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import produceReducer from "./produce";
+
+
+// ./src/store/index.js
+// ...
+const rootReducer = combineReducers({
+    produce: produceReducer
+});
+
+
+let enhancer 
+
+if(process.env.NODE_ENV !== "production") {
+    const logger = require("redux-logger").default;
+
+    const composeEnhancers = 
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(logger));
+}
+
+
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer)
+}
+
+
+export default configureStore;
